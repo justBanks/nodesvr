5ee916cd6fe10e50fde410e0c4c3844d42904b08
justBanks <Darryl.Banks@gmail.com>
2019-11-27 17:21:05 -0500

    enable CORS

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
  
-app.use(express.json());
+app.use(express.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send({"message": "Hello, world"})
 })
  
 app.post('/', function(req, res){
-	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json')
 	res.send(req.body)
 })
 
-app.listen(3000)
+app.listen()
