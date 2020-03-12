b64d521199652a8ce3f0b07773ec96e7b39e46f8
justBanks <Darryl.Banks@gmail.com>
2020-03-11 22:33:56 -0700

    fix

--- api.js
+++ api.js
@@ -19,10 +19,10 @@ const connected_app = 'https://nodesvr.herokuapp.com'
 var org = nforce.createConnection({
   environment: 'sandbox',
   mode: 'single',
-  username: '(CONFIDENTIAL)',
-  password: '(CONFIDENTIAL)',
-  clientId: '(CONFIDENTIAL)',
-  clientSecret: '(CONFIDENTIAL)',
+  username: 'dlab.usr@gmail.com',
+  password: 'pw8chars',
+  clientId: '3MVG9oZtFCVWuSwPENXV_adRl8Q1TtPA9SFpGB0c6SlXpnzMJptTplD0uA..426Xg_0xPPsOYCr_5wCRWFsz6',
+  clientSecret: '56611D9CE2BABC2009BF31B57D81E0A9AAE36F21EBD61CBC23ED71609FDAF8CF',
   redirectUri: `${connected_app}/oauth/_callback`,
   // oauth: oauth,
 })
